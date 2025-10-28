39a5a97c6ab2ce0903af4652ff9979acb6657d7b
sam3313943 <sansinstitute41@gmail.com>
2025-10-28 15:03:35 +0000

    app.py

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route(‘/‘)
+@app.route('/')
 def index():
-    return render_template(‘index.html’)
+    return render_template('index.html')
 
-@app.route(‘/result’, methods=[‘POST’])
+@app.route('/result', methods=['POST'])
 def result():
-    reg_no = request.form[‘reg_no’]
+    reg_no = request.form['reg_no']
     # Just for testing
     result_data = {“SAMS001”: “Passed”, “SAMS002”: “Failed”}
     result = result_data.get(reg_no.upper(), “Result not found”)
-    return render_template(‘result.html’, reg_no=reg_no, result=result)
+    return render_template('result.html', reg_no=reg_no, result=result)
 
 if __name__ == “__main__”:
     app.run(debug=True)
