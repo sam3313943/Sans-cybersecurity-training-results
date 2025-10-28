424ebdd448811a26bc88f46c31d5611152f315d2
sam3313943 <sansinstitute41@gmail.com>
2025-10-28 13:08:31 +0000

    Add app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route(‘/‘)
+def index():
+    return render_template(‘index.html’)
+
+@app.route(‘/result’, methods=[‘POST’])
+def result():
+    reg_no = request.form[‘reg_no’]
+    # Just for testing
+    result_data = {“SAMS001”: “Passed”, “SAMS002”: “Failed”}
+    result = result_data.get(reg_no.upper(), “Result not found”)
+    return render_template(‘result.html’, reg_no=reg_no, result=result)
+
+if __name__ == “__main__”:
+    app.run(debug=True)
+  Add app.py
